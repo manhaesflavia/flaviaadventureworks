2922cf4c0e5c84f322a54a8bcfecb2b57aa06a1d
flaviamanhaes <flaviamanhaes302@gmail.com>
2024-08-12 16:47:03 -0300

    Staging: productcategory.sql

--- models/staging/stg__productcategory.sql
+++ models/staging/stg__productcategory.sql
@@ -1,6 +1,6 @@
 with source as (
     select
-        as pk_productsubcategory as pk_productcategory
+        productcategoryid as pk_productcategory
         , name as productcategory_name
     from {{ source('adventure_works_seeds','productcategory') }}
 )
